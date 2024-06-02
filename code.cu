cce983e32c13ef0b29e850fb4e9ef0b0a19594a5
Avenfenix <thedonjuan008p@gmail.com>
2024-06-02 11:45:53 -0400

    Update code.cu

--- code.cu
+++ code.cu
@@ -1,3 +1,5 @@
+%%cuda
+
 #include <iostream>
 #include <time.h>
 #include <cuda_runtime.h>
@@ -57,8 +59,8 @@ void funcionCPU(float *R, float *G, float *B, float *Rout, float *Gout, float *B
 		int nueva_pieza = Orden[pieza];
 		
 		// Posicion final del pixel
-		int fi = (nueva_pieza % P ) * S + i % S;
-		int fj = (nueva_pieza / P ) * S + j % S;
+		int fi = ( nueva_pieza % P ) * S + pi;
+		int fj = ( nueva_pieza / P ) * S + pj;
 
 		// Indexar
 		int Idx_nueva = fj * N + fi;
@@ -98,101 +100,96 @@ __global__ void kernelGPU(float *R, float *G, float *B, float *Rout, float *Gout
 	
 }
 
-__global__ void emptyArray(float *R, float *G, float *B, int N){
-	int Idx = threadIdx.x + blockDim.x * blockIdx.x;
-	if (Idx < N*N){
-		R[Idx] = 0;
-		G[Idx] = 0;
-		B[Idx] = 0;
-	}
-}
-
 int main(int argc, char **argv){
-	if (argc != 4){
-		return 0;
-	}
 	int N, S;
 	int *Ohost, *Odev;
-    float *Rhost, *Ghost, *Bhost;
-    float *Rhostout, *Ghostout, *Bhostout;
+  float *Rhost, *Ghost, *Bhost;
+  float *Rhostout, *Ghostout, *Bhostout;
 	float *Rdev, *Gdev, *Bdev;
-    float *Rdevout, *Gdevout, *Bdevout;
-    Read(&Rhost, &Ghost, &Bhost, &N, &S, &Ohost, argv[1]);
+  float *Rdevout, *Gdevout, *Bdevout;
+  
+  char names[5][3][20] = {
+        {"img100x100.txt", "img100x100CPU.txt", "img100x100GPU.txt"},
+        {"img200x200.txt", "img200x200CPU.txt", "img200x200GPU.txt"},
+        {"img400x400.txt", "img400x400CPU.txt", "img400x400GPU.txt"},
+        {"img800x800.txt", "img800x800CPU.txt", "img800x800GPU.txt"},
+        {"img1600x1600.txt", "img1600x1600CPU.txt", "img1600x1600GPU.txt"}};
+ for(int i = 0; i < 5; i++) {
+    Read(&Rhost, &Ghost, &Bhost, &N, &S, &Ohost, names[i][0]);
+    // CPU
+    Rhostout = (float*)malloc(N*N*sizeof(float));
+    Ghostout = (float*)malloc(N*N*sizeof(float));
+    Bhostout = (float*)malloc(N*N*sizeof(float));
+    
+    clock_t t1, t2;
+    t1 = clock();
 
-	// CPU
-	Rhostout = (float*)malloc(N*N*sizeof(float));
-	Ghostout = (float*)malloc(N*N*sizeof(float));
-	Bhostout = (float*)malloc(N*N*sizeof(float));
-	
-	clock_t t1, t2;
-	t1 = clock();
+    // Procesar imagen
+    funcionCPU(Rhost, Ghost, Bhost, Rhostout, Ghostout, Bhostout, N, S, Ohost); 
 
-	// Procesar imagen
-	funcionCPU(Rhost, Ghost, Bhost, Rhostout, Ghostout, Bhostout, N, S, Ohost); 
+    t2 = clock();
+    double dif_cpu = 1000.0 * (double) (t2 - t1) / CLOCKS_PER_SEC;
+    printf("Tiempo CPU %s: %f [ms]\n", names[i][0], dif_cpu);
 
-	t2 = clock();
-	double dif_cpu = 1000.0 * (double) (t2 - t1) / CLOCKS_PER_SEC;
-	printf("Tiempo CPU %s: %f [ms]\n", argv[1], dif_cpu);
+    // Guardar imagen salida y liberar memoria
+    Write(Rhostout, Ghostout, Bhostout, N, names[i][1]);
+    delete[] Rhostout; delete[] Ghostout; delete[] Bhostout;
+    
+    // GPU
+    int grid_size, block_size = 256;
+    grid_size = (int)ceil((float) N * N / block_size);
+    
 
-	// Guardar imagen salida y liberar memoria
-	Write(Rhostout, Ghostout, Bhostout, N, argv[2]);
-	delete[] Rhostout; delete[] Ghostout; delete[] Bhostout;
-	
-	// GPU
-	int grid_size, block_size = 256;
-	grid_size = (int)ceil((float) N * N / block_size);
-	
+    // Reservar memoria imagen GPU
+    cudaMalloc((void**)&Rdev, N * N * sizeof(float));
+    cudaMalloc((void**)&Gdev, N * N * sizeof(float));
+    cudaMalloc((void**)&Bdev, N * N * sizeof(float));
 
-	// Reservar memoria imagen GPU
-	cudaMalloc((void**)&Rdev, N * N * sizeof(float));
-	cudaMalloc((void**)&Gdev, N * N * sizeof(float));
-	cudaMalloc((void**)&Bdev, N * N * sizeof(float));
+    // Copiar imagen CPU a imagen GPU
+    cudaMemcpy(Rdev, Rhost, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Gdev, Ghost, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Bdev, Bhost, N * N * sizeof(float), cudaMemcpyHostToDevice);
+    
+    // Reservar memoria imagen salida GPU
+    cudaMalloc((void**)&Rdevout, N * N * sizeof(float));
+    cudaMalloc((void**)&Gdevout, N * N * sizeof(float));
+    cudaMalloc((void**)&Bdevout, N * N * sizeof(float));
 
-	// Copiar imagen CPU a imagen GPU
-	cudaMemcpy(Rdev, Rhost, N * N * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(Gdev, Ghost, N * N * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(Bdev, Bhost, N * N * sizeof(float), cudaMemcpyHostToDevice);
-	
-	// Reservar memoria imagen salida GPU
-	cudaMalloc((void**)&Rdevout, N * N * sizeof(float));
-	cudaMalloc((void**)&Gdevout, N * N * sizeof(float));
-	cudaMalloc((void**)&Bdevout, N * N * sizeof(float));
+    // Reservar memoria y copiar orden CPU a orden GPU
+    cudaMalloc((void**)&Odev, (N/S)*(N/S)*sizeof(int));
+    cudaMemcpy(Odev, Ohost, (N/S)*(N/S)*sizeof(int), cudaMemcpyHostToDevice);
 
-	// Reservar memoria y copiar orden CPU a orden GPU
-	cudaMalloc((void**)&Odev, (N/S)*(N/S)*sizeof(int));
-	cudaMemcpy(Odev, Ohost, (N/S)*(N/S)*sizeof(int), cudaMemcpyHostToDevice);
+    cudaEvent_t ct1, ct2;
+    float dif_gpu;
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
 
-	cudaEvent_t ct1, ct2;
-	float dif_gpu;
-	cudaEventCreate(&ct1);
-	cudaEventCreate(&ct2);
-	cudaEventRecord(ct1);
+    // Procesar imagen
+    kernelGPU<<<grid_size, block_size>>>(Rdev, Gdev, Bdev, Rdevout, Gdevout, Bdevout, N, S, Odev);
+    cudaDeviceSynchronize();
 
-	// Procesar imagen
-	kernelGPU<<<grid_size, block_size>>>(Rdev, Gdev, Bdev, Rdevout, Gdevout, Bdevout, N, S, Odev);
-	cudaDeviceSynchronize();
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&dif_gpu, ct1, ct2);
+    printf("Tiempo GPU %s: %f [ms]\n", names[i][0], dif_gpu);
 
-	cudaEventRecord(ct2);
-	cudaEventSynchronize(ct2);
-	cudaEventElapsedTime(&dif_gpu, ct1, ct2);
-	printf("Tiempo GPU %s: %f [ms]\n", argv[1], dif_gpu);
+    // Reservar memoria imagen salida CPU
+    Rhostout = (float*)malloc(N*N*sizeof(float));
+    Ghostout = (float*)malloc(N*N*sizeof(float));
+    Bhostout = (float*)malloc(N*N*sizeof(float));
 
-	// Reservar memoria imagen salida CPU
-	Rhostout = (float*)malloc(N*N*sizeof(float));
-	Ghostout = (float*)malloc(N*N*sizeof(float));
-	Bhostout = (float*)malloc(N*N*sizeof(float));
+    // Copiar imagen salida GPU a imagen salida CPU
+    cudaMemcpy(Rhostout, Rdevout, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(Ghostout, Gdevout, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(Bhostout, Bdevout, N * N * sizeof(float), cudaMemcpyDeviceToHost);
 
-	// Copiar imagen salida GPU a imagen salida CPU
-	cudaMemcpy(Rhostout, Rdevout, N * N * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(Ghostout, Gdevout, N * N * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(Bhostout, Bdevout, N * N * sizeof(float), cudaMemcpyDeviceToHost);
-
-	// Guardar imagen salida y liberar memoria
-	Write(Rhostout, Ghostout, Bhostout, N, argv[3]);
-	free(Rhost); free(Ghost); free(Bhost); free(Ohost);
-	free(Rhostout); free(Ghostout); free(Bhostout);
-	cudaFree(Rdev); cudaFree(Gdev); cudaFree(Bdev);  cudaFree(Odev);
-	cudaFree(Rdevout); cudaFree(Gdevout); cudaFree(Bdevout);
-
+    // Guardar imagen salida y liberar memoria
+    Write(Rhostout, Ghostout, Bhostout, N, names[i][2]);
+    free(Rhost); free(Ghost); free(Bhost); free(Ohost);
+    free(Rhostout); free(Ghostout); free(Bhostout);
+    cudaFree(Rdev); cudaFree(Gdev); cudaFree(Bdev);  cudaFree(Odev);
+    cudaFree(Rdevout); cudaFree(Gdevout); cudaFree(Bdevout);
+  }
 	return 0;
 }
